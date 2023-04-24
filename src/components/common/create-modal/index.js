7132a74cef4cd61c9c90279742232d98381db013
Kapil <kapil@weblliasto.com>
2023-04-25 00:31:42 +0530

    feature : input field added in create form

--- src/components/common/create-modal/index.js
+++ src/components/common/create-modal/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../button";
-import { Modal } from "react-bootstrap";
+import { Form, InputGroup, Modal } from "react-bootstrap";
 
 const CreateModal = ({ open, close, ...props }) => {
   return (
@@ -16,7 +16,24 @@ const CreateModal = ({ open, close, ...props }) => {
           Create Your Todo
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body></Modal.Body>
+      <Modal.Body className="my-auto mx-auto w-50 py-5 px-5">
+        <InputGroup size="sm" className="mb-3">
+          <InputGroup.Text id="inputGroup-sizing-sm">Task Name</InputGroup.Text>
+          <Form.Control
+            type="text"
+            aria-label="Small"
+            aria-describedby="inputGroup-sizing-sm"
+          />
+        </InputGroup>
+        <InputGroup size="sm" className="mb-3">
+          <InputGroup.Text id="inputGroup-sizing-sm">Comment</InputGroup.Text>
+          <Form.Control
+            type="text"
+            aria-label="Small"
+            aria-describedby="inputGroup-sizing-sm"
+          />
+        </InputGroup>
+      </Modal.Body>
       <Modal.Footer>
         <Button onClick={close}>Close</Button>
       </Modal.Footer>
