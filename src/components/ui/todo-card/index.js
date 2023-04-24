1e23179a5c3ac645ed5a6510bca65090d9d0632a
Kapil <kapil@weblliasto.com>
2023-04-25 01:51:32 +0530

    feature : added dynamic data in card

--- src/components/ui/todo-card/index.js
+++ src/components/ui/todo-card/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-const TodoCard = () => {
+const TodoCard = ({ data }) => {
   return (
     <Card style={{ width: "18rem", margin: " 30px" }}>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Title>Todo Details</Card.Title>
+        <p>
+          Task Name : <span>{data.taskName}</span>
+        </p>
+        <p>
+          Comment <span>{data.comment}</span>
+        </p>
+        <p>
+          Date <span>{data.date}</span>
+        </p>
       </Card.Body>
     </Card>
   );
